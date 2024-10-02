a98772d98c8966a355c4783075fc3ef7d821d3c1
MaximilianMeiler <113135205+MaximilianMeiler@users.noreply.github.com>
2024-10-02 11:14:11 -0400

    Code finalization

--- README.md
+++ README.md
@@ -1,22 +1,8 @@
-<h1>REST API Workshop</h1>
-<h2>This is a repo that I used to lead the REST API workshop for the UF Software Engineering Club</h2>
-<br/>
-<h2>Run the app locally</h2>
-<p>(Download Node)</p>
-<ul>
-    <li>npm i</li>
-    <li>node index.js</li>
-</ul>
-<h2>Contanerize the app and run the container locally</h2>
-<p>(Download Docker)</p>
-<ul>
-    <li>docker build -t express ./</li>
-    <li>docker run -p 3000:3000 express</li>
-</ul>
-<h2>Push the image to google cloud</h2>
-<p>Create a service account in google cloud and copy/paste the json key into gcloudKey.json</p>
-<ul>
-    <li>docker login -u _json_key -p "$(cat ./gcloudKey.json)" https://gcr.io</li>
-    <li>docker build -t gcr.io/[project-id]/express ./</li>
-    <li>docker push gcr.io/[project-id]/express</li>
-</ul>
+## Requirements
+
+## Running the code
+
+Server:
+```
+nodemon server.js
+```
--- server.js
+++ server.js
@@ -1,40 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+cors = require('cors');
+
+
+const app = express();
+app.use(cors({origin: '*'}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+ })
+  
+
+const facts = [
+  "Dogs have three eyelids, including one that helps keep their eyes moist and protected.",
+  "The Norwegian Lundehund is the only dog breed with six toes on each foot.",
+  "Dogs can detect certain types of cancer, such as prostate cancer, with up to 98% accuracy.",
+  "The Basenji is the only breed of dog that can't bark, but they can still make a variety of other vocalizations.",
+  "The Labrador Retriever is the most popular breed of dog in the United States, Canada, and the United Kingdom.",
+  "Dogs can understand over 100 words and gestures, and some can even count up to five.",
+  "The smallest dog breed in the world is the Chihuahua, which can weigh as little as two pounds.",
+  "Dogs can get jealous and exhibit behaviors similar to human jealousy, such as attention-seeking and aggression.",
+  "Dogs' noses are up to 100,000 times more sensitive than humans, which allows them to detect odors we can't even perceive.",
+  "The tallest dog breed in the world is the Great Dane, which can stand over three feet tall at the shoulder."
+]
 
 
 app.get('/', (req, res) => {
- res.send(facts)
+ res.send(facts);
 })
 
-
-app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
-})
-
-const facts = {
-  "0": "Dogs have three eyelids, including one that helps keep their eyes moist and protected.",
-  "1": "The Norwegian Lundehund is the only dog breed with six toes on each foot.",
-  "2": "Dogs can detect certain types of cancer, such as prostate cancer, with up to 98% accuracy.",
-  "3": "The Basenji is the only breed of dog that can't bark, but they can still make a variety of other vocalizations.",
-  "4": "The Labrador Retriever is the most popular breed of dog in the United States, Canada, and the United Kingdom.",
-  "5": "Dogs can understand over 100 words and gestures, and some can even count up to five.",
-  "6": "The smallest dog breed in the world is the Chihuahua, which can weigh as little as two pounds.",
-  "7": "Dogs can get jealous and exhibit behaviors similar to human jealousy, such as attention-seeking and aggression.",
-  "8": "Dogs' noses are up to 100,000 times more sensitive than humans, which allows them to detect odors we can't even perceive.",
-  "9": "The tallest dog breed in the world is the Great Dane, which can stand over three feet tall at the shoulder."
- }
- 
- 
  app.get('/random', (req, res) => {
-  const randomNum = Math.floor(Math.random() * 10);
-  res.send(facts[randomNum])
+  const randomNum = Math.floor(Math.random() * facts.length);
+  res.send(facts[randomNum]);
  })
  
- 
  app.get('/fact', (req, res) => {
-  const parameters = req.query
-  const i = parameters.index
-  res.send(facts[i])
+  const parameters = req.query;
+  const i = parameters.index;
+  res.send(facts[i]);
+ })
+
+ app.post('/new', (req, res) => {
+  facts.push(req.body.fact);
+  res.send(`New fact added at index ${facts.length-1}!`)
  })
  
